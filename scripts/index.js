4112f7cb35f63640bac13bfe779b6d097149c6cc
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-27 19:34:52 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -25,31 +25,14 @@ class student {
 
 let results = [];
 
-let  = [ {
-    "question": "Name this species of bird.",
-    "image" : "images/barn-owl-2550068__340.webp",
-    "answer": "Barn Owl"
-    },
-    {
-    "question": "Name this species of bird.",
-    "image" : "images/dove-2516641__340.webp",
-    "answer": "Dove"
-    },
-    {
-    "question": "Name this species of bird.",
-    "image" : "images/hummingbird-2139279__340.webp",
-    "answer": "Hummingbird"
-    }
-  ];
-
 // Make the database point to the location root -> deck -> flashcards
 // If the location doesn't exist is will be created
-let dbLocation = firebase.database().ref('deck/flashcards'); 
+let dbLocation = firebase.database().ref('deck/results'); 
  
 // myFlashcards will be stored under flashcards in the database
 // Anything that was in this location will be overwritten
 // Thus, a write operation also does an update
-dbLocation.set(myFlashcards);
+dbLocation.set(results);
 
 // As before,make the database point to the location where the data exists
 // If the location doesn't exist it will be created but there will be nothing to retrieve
