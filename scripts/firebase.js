f3dee56d47e9be49b140a394cf0ab48c61b7ff83
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-29 17:54:05 -0400

    Create firebase.js

---
+++ scripts/firebase.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD_M1PPIlNIG0yEmZeDlptwApOjY3oFr70",
+    authDomain: "learning-tool-5eb63.firebaseapp.com",
+    databaseURL: "https://learning-tool-5eb63-default-rtdb.firebaseio.com",
+    projectId: "learning-tool-5eb63",
+    storageBucket: "learning-tool-5eb63.appspot.com",
+    messagingSenderId: "128932611744",
+    appId: "1:128932611744:web:626d046619028126fcebc5"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
