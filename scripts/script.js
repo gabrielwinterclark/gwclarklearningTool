78dfb0331c3ee5723ddd180b15f1984d98d8137c
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-29 18:34:05 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -24,6 +24,8 @@ window.addEventListener('beforeunload', function (e) {
     inputListener()
 });
 
+//home
+
 let form = document.getElementById("my-form")
 form.addEventListener("submit", () => {
 	event.preventDefault()
@@ -47,4 +49,26 @@ form.addEventListener("submit", () => {
     inputs.id = inputRef.id
 
     inputRef.set(inputs)
+})
+
+//LO1
+
+let lo1-q1-a-input = document.getElementById("lo1-q1-a-input")
+form.addEventListener("lo1-q1-a-input", () => {
+	event.preventDefault()
+	console.log("hey")
+
+    let lo1-q1-answer-a = document.getElementById("lo1-q1-a-input").value
+
+    let loq-q1-inputs = {
+        id: "",
+        lo1-q1-answer,
+        userId: "userID"
+    }
+
+    let inputRef = firestore.collection("lo1-q1-inputs").doc()
+
+    lo1-q1-inputs.id = inputRef.id
+
+    inputRef.set(lo1-q1-inputs)
 })
