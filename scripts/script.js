31efe6d45b40f3e2922606b6c34d3a9873e4ca86
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-07-29 18:43:41 -0400

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -52,25 +52,3 @@ form.addEventListener("submit", () => {
 
     inputRef.set(inputs);
 })
-
-//LO1
-
-let lo1-q1-a-input = document.getElementById("lo1-q1-a-input")
-form.addEventListener("lo1-q1-a-input", () => {
-	event.preventDefault()
-	console.log("hey")
-
-    let lo1-q1-answer-a = document.getElementById("lo1-q1-a-input").value
-
-    let loq-q1-inputs = {
-        id: "",
-        lo1-q1-answer,
-        userId: "userID"
-    }
-
-    let inputRef = firestore.collection("lo1-q1-inputs").doc()
-
-    lo1-q1-inputs.id = inputRef.id
-
-    inputRef.set(lo1-q1-inputs)
-})
