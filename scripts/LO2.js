8bb98e42494de91907f6ba354125f422a24d9a76
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-01 15:51:11 -0400

    Update LO2.js

--- scripts/LO2.js
+++ scripts/LO2.js
@@ -4,13 +4,13 @@ console.log("iteration 1")
 
 //question 1
 
-let q1feedback = document.getElementById("lo1-q1-feedback")
+let q1feedback = document.getElementById("lo2-q1-feedback")
 
-let form1 = document.getElementById("lo1-q1-form")
+let form1 = document.getElementById("lo2-q1-form")
 form1.addEventListener("submit", () => {
     event.preventDefault()
 
-    let value = document.querySelector('input[name="lo1-q1-answer"]:checked').value
+    let value = document.querySelector('input[name="lo2-q1-answer"]:checked').value
 
     console.log("value = " + value)
 
@@ -18,29 +18,29 @@ form1.addEventListener("submit", () => {
 
     switch(value) {
         case "Correct":
-            q1feedback.innerHTML = "Correct! Yes, Jeremiah values playing the cello as an end, for itself."
+            q1feedback.innerHTML = "Correct! Yes, Jorge values BMWs for a nonrelational property: the well-made nature of the cars can’t be separated from them."
             break;
         case "Incorrect":
-            q1feedback.innerHTML = "Not quite. Remember, something has intrinsic value when it is valuable for itself. Jeremiah does, in fact, value playing the cello for itself: he values it intrinsically."
+            q1feedback.innerHTML = "Not quite. Remember, something has intrinsic value by this second definition when it is valued for its nonrelational properties, or those properties that can’t be separated from it. Is a car being well made separable from the car itself?"
             break;
     }
 
     // firestore.collection("answers").doc("userId").collection("questions").doc("lo1-q1").set(q1)
-    firestore.collection("answers").doc(auth.currentUser.uid).collection("questions").doc("lo1-q1").set({
-        q1: value,
+    firestore.collection("answers").doc(auth.currentUser.uid).collection("questions").doc("lo2-q1").set({
+        q4: value,
     })
 
 })
 
 //question 2
 
-let q2feedback = document.getElementById("lo1-q2-feedback")
+let q2feedback = document.getElementById("lo2-q2-feedback")
 
-let form2 = document.getElementById("lo1-q2-form")
+let form2 = document.getElementById("lo2-q2-form")
 form2.addEventListener("submit", () => {
     event.preventDefault()
 
-    let value = document.querySelector('input[name="lo1-q2-answer"]:checked').value
+    let value = document.querySelector('input[name="lo2-q2-answer"]:checked').value
 
     console.log("value = " + value)
 
@@ -48,55 +48,46 @@ form2.addEventListener("submit", () => {
 
     switch(value) {
         case "Incorrect":
-            q2feedback.innerHTML = "Incorrect. Think about it: does May value playing the cello for itself, or for something else? In this case, it looks like May values playing the cello in service of something else. Read on to learn what type of value May assigns to playing the cello!"
+            q2feedback.innerHTML = "Incorrect. Micah loves the movie because it makes them and their friends laugh. Is this a nonrelational property of the movie, or is it something that can be separated from the movie? "
             break;
         case "Correct":
-            q2feedback.innerHTML = "Correct! You may be wondering in what way May values playing the cello. Read on to find out."
+            q2feedback.innerHTML = "Correct! You may be wondering in what way Micah values Ratatouille. Read on to find out."
             break;
     }
 
     // firestore.collection("answers").doc("userId").collection("questions").doc("lo1-q1").set(q1)
-    firestore.collection("answers").doc(auth.currentUser.uid).collection("questions").doc("lo1-q2").set({
-        q2: value,
+    firestore.collection("answers").doc(auth.currentUser.uid).collection("questions").doc("lo2-q2").set({
+        q5: value,
     })
 
 })
 
 //question 3
 
-let q3feedback = document.getElementById("lo1-q3-feedback")
+let q3feedback = document.getElementById("lo2-q3-feedback")
 
-let form3 = document.getElementById("lo1-q3-form")
+let form3 = document.getElementById("lo2-q3-form")
 form3.addEventListener("submit", () => {
     event.preventDefault()
 
-    let value = document.querySelector('input[name="lo1-q3-answer"]:checked').value
+    let value = document.querySelector('input[name="lo2-q3-answer"]:checked').value
 
     console.log("value = " + value)
 
     //return
 
     switch(value) {
-        case "Incorrect (A)":
-            q3feedback.innerHTML = "You are partially correct. Aminah does value her master’s degree instrumentally, as a means to move towards her future career. But, is there anyone else who also instrumentally values their degree?"
+        case "Incorrect":
+            q3feedback.innerHTML = "Incorrect. For Wes to value donating blood intrinsically in this way, he would have to value it for some nonrelational (inseparable) property. Is donating blood separable from the fact that it helps people in need? Remember, one way to test if something has intrinsic value is to think about it in a vacuum."
             break;
-        case "Incorrect (B)":
-            q3feedback.innerHTML = "Incorrect. Phoebe values her master’s degree intrinsically for itself."
-            break;
-        case "Incorrect (C)":
-            q3feedback.innerHTML = "You are partially correct. Sam does value his master’s degree instrumentally, as a means to eventually write a book about environmental science. But, is there anyone else who also instrumentally values their degree? "
-            break;
-        case "Correct (D)":
-            q3feedback.innerHTML = "Correct! Yes, both Aminah and Sam value their degrees instrumentally, as a means to achieve their future goals."
-            break;
-        case "Incorrect (E)":
-            q3feedback.innerHTML = "Not quite. Not all of the students instrumentally value their degree as a means to some other end. Think about who might value their degree intrinsically."
-            break;            
+        case "Correct":
+            q3feedback.innerHTML = "Correct! Wes values donating blood extrinsically because he values it for the fact that it helps people, which is a relational (separable) property."
+            break;      
     }
 
     // firestore.collection("answers").doc("userId").collection("questions").doc("lo1-q1").set(q1)
-    firestore.collection("answers").doc(auth.currentUser.uid).collection("questions").doc("lo1-q3").set({
-        q3: value,
+    firestore.collection("answers").doc(auth.currentUser.uid).collection("questions").doc("lo2-q3").set({
+        q6: value,
     })
 
 })
