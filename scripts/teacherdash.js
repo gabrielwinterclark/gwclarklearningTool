68073ae1801daedc986203f933fbfbbfc72b7022
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-01 22:53:12 -0400

    Update teacherdash.js

--- scripts/teacherdash.js
+++ scripts/teacherdash.js
@@ -1,34 +1,13 @@
-console.log("yeehaw 2")
+console.log("yeehaw 7")
 
-const inputsDiv = document.getElementById("inputs")
+const inputsDiv = document.getElementById("inputs");
 
-const inputListener = firestore.collection("answers").doc("userId").collection("questions").doc("home")
-    .onSnapshot(doc => {
-        console.log("inputs listener")
-
-        if(!doc.exists)
-            return
-
-        inputsDiv.innerHTML = "";
-
-        let homeData = doc.data();
-
-        inputsDiv.innerHTML = "Name: " + homeData.name + ", " + homeData.motivation;
-
-        querySnapshot.forEach(doc => {
-        console.log(doc.data())
-
-        let input = doc.data()
-
-        let inputsElement = document.createElement("p")
+const inputListener = firestore.database().ref();
+dbRef.child("users").child(userId).get().then((snapshot) => { 
+    if (snapshot.exists()) {
+        let input = doc.data();
+        let inputsElement = document.createElement("p");
         inputsElement.innerHTML = "Name: " + input.name + " || Motivation = " + input.motivation
-            
         inputsDiv.appendChild(inputsElement)
-        })
     })
-
-window.addEventListener('beforeunload', function (e) {
-    e.preventDefault();
-    e.returnValue = '';
-    inputListener()
 });
