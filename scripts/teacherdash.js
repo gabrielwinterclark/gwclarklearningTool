2ffc65b9a8d1e7be3a9bed353588995194112716
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-01 23:20:36 -0400

    Update teacherdash.js

--- scripts/teacherdash.js
+++ scripts/teacherdash.js
@@ -1,10 +1,10 @@
-console.log("yeehaw 10")
+console.log("yeehaw 11")
 
 const db = firebase.firestore(); 
 
 db.collection('answers').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
-        console.log(doc)
+        console.log(doc.data())
 })
 })
 //////////////////////////////////////////////////
