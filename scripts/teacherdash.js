02c75f610342d82af1f3f176754031eaebc557bd
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-01 23:31:41 -0400

    Update teacherdash.js

--- scripts/teacherdash.js
+++ scripts/teacherdash.js
@@ -1,10 +1,28 @@
-console.log("yeehaw 11")
+console.log("yeehaw 12")
 
 const results = document.getElementById('inputs');
 
+//create element and render
+
+function renderAnswers(doc){
+  let li = document.createElement('li');
+  let name = document.createElement('span');
+    let motivation = document.createElement('span');
+    
+  li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    motivation.textContent = doc.data().motivation;
+    
+    li.appendChild(name);
+    li.appendChild(motivation);
+    
+    results.appendChild(li);
+    
+}
+
 db.collection('answers').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
-        console.log(doc.data())
+        renderAnswers(doc);        
 })
 })
 //////////////////////////////////////////////////
