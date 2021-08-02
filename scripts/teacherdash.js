ec608dd4883e84df151a960464a40ee7be0009e1
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-01 23:10:40 -0400

    Update teacherdash.js

--- scripts/teacherdash.js
+++ scripts/teacherdash.js
@@ -1,17 +1,9 @@
-console.log("yeehaw 8")
+console.log("yeehaw 9")
 
-const inputsDiv = document.getElementById("inputs");
+const db = firebase.firestore(); 
 
-const inputListener = firestore.database().ref();
-dbRef.child("users").child(userId).get().then((snapshot) => { 
-    if (snapshot.exists()) {
-        let input = doc.data();
-        let inputsElement = document.createElement("p");
-        inputsElement.innerHTML = "Name: " + input.name + " || Motivation = " + input.motivation
-        inputsDiv.appendChild(inputsElement)
-    } else {
-        console.log("No data available");
-    }    
-    }).catch((error) => {
-        console.error(error);
-});
+db.collection('answers').get().then((snapshot) => {
+    console.log(snapshot.docs);
+})
+
+//////////////////////////////////////////////////
