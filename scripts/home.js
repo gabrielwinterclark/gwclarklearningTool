127635b3916b117848a09a1efcb8c699ea34520e
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-01 21:41:36 -0400

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -33,30 +33,3 @@ form.addEventListener("submit", () => {
 })
 
 //retrieving data
-
-const inputsDiv = document.getElementById("inputs")
-
-const inputListener = firestore.collection("inputs")
-    .onSnapshot(querySnapshot => {
-        console.log("inputs listener")
-
-        inputsDiv.innerHTML = ""
-
-	querySnapshot.forEach(doc => {
-		console.log(doc.data())
-		
-        let input = doc.data()
-
-let inputsElement = document.createElement("p")
-inputsElement.innerHTML = "Name: " + input.name + " | | Motivation = " + input.motivation
-		
-		inputsDiv.appendChild(inputsElement)
-    
-         })
-    })
-
-window.addEventListener('beforeunload', function (e) {
-    //e.preventDefault();
-    //e.returnValue = '';
-    inputListener()
-});
