5d79dc9f966600a3ed9ef2d2fb4466e70e94bdbf
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-01 21:03:27 -0400

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -1,38 +1,5 @@
 console.log("version 4")
 
-const inputsDiv = document.getElementById("inputs")
-
-const inputListener = firestore.collection("answers").doc("userId").collection("questions").doc("home")
-    .onSnapshot(doc => {
-        console.log("inputs listener")
-
-        if(!doc.exists)
-            return
-
-        inputsDiv.innerHTML = ""
-
-        let homeData = doc.data()
-
-        inputsDiv.innerHTML = "Name: " + homeData.name + ", " + homeData.motivation
-
-        querySnapshot.forEach(doc => {
-        console.log(doc.data())
-
-       let input = doc.data()
-
-        let inputsElement = document.createElement("p")
-             inputsElement.innerHTML = "Name: " + input.name + " || Motivation = " + input.motivation
-            
-             inputsDiv.appendChild(inputsElement)
-         })
-    })
-
-window.addEventListener('beforeunload', function (e) {
-    e.preventDefault();
-    e.returnValue = '';
-    inputListener()
-});
-
 //home
 
 var thankyou = document.getElementById("thankyou");
@@ -66,3 +33,38 @@ form.addEventListener("submit", () => {
     // VHxJJc0cGOV0Xt7kH8ZTjAgdy6V2
     
 })
+
+//retrieving data
+
+const inputsDiv = document.getElementById("inputs")
+
+const inputListener = firestore.collection("answers").doc("userId").collection("questions").doc("home")
+    .onSnapshot(doc => {
+        console.log("inputs listener")
+
+        if(!doc.exists)
+            return
+
+        inputsDiv.innerHTML = ""
+
+        let homeData = doc.data()
+
+        inputsDiv.innerHTML = "Name: " + homeData.name + ", " + homeData.motivation
+
+        querySnapshot.forEach(doc => {
+        console.log(doc.data())
+
+       let input = doc.data()
+
+        let inputsElement = document.createElement("p")
+             inputsElement.innerHTML = "Name: " + input.name + " || Motivation = " + input.motivation
+            
+             inputsDiv.appendChild(inputsElement)
+         })
+    })
+
+window.addEventListener('beforeunload', function (e) {
+    e.preventDefault();
+    e.returnValue = '';
+    inputListener()
+});
