34d8fc8206fd379c7fc2f5afd7995df250648027
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-02 01:19:36 -0400

    Update teacherdash.js

--- scripts/teacherdash.js
+++ scripts/teacherdash.js
@@ -1,63 +1,30 @@
 console.log("yeehaw 24")
 
-// const results = document.getElementById('inputs');
+const results = document.getElementById('inputs');
 
-// 	  const db = firebase.firestore();
+	  const db = firebase.firestore();
 
-// //create element and render
+//create element and render
 
-// function renderAnswers(doc){
-//   let li = document.createElement('li');
-//   let name = document.createElement('span');
-//   let motivation = document.createElement('span');
+function renderAnswers(doc){
+  let li = document.createElement('li');
+  let name = document.createElement('span');
+  let motivation = document.createElement('span');
     
-//   li.setAttribute('data-id', doc.id);
-//     name.textContent = doc.Collection("answers").Doc("home").name;
-//     motivation.textContent = doc.Collection("answers").Doc("home").motivation;
+  li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    motivation.textContent = doc.data().motivation;
     
-//     li.appendChild(name);
-//     li.appendChild(motivation);
+    li.appendChild(name);
+    li.appendChild(motivation);
     
-//     results.appendChild(li);
+    results.appendChild(li);
     
-// }
+}
 
-// db.collection("answers").get().then((snapshot) => {
-//     snapshot.docs.forEach(doc => {
-//         renderAnswers(doc);        
-// })
-// })
-// //////////////////////////////////////////////////
-
-const inputsDiv = document.getElementById("inputs")
-
-const inputListener = firestore.collection("answers").doc("userId").collection("questions").doc("home")
-    .onSnapshot(doc => {
-        console.log("inputs listener")
-
-        if(!doc.exists)
-            return
-
-        inputsDiv.innerHTML = ""
-
-        let homeData = doc.data()
-
-        inputsDiv.innerHTML = "Name: " + homeData.name + ", " + homeData.motivation
-
-        querySnapshot.forEach(doc => {
-             console.log(doc.data())
-
-            let input = doc.data()
-
-           let inputsElement = document.createElement("p")
-            inputsElement.innerHTML = "Name: " + input.name + " || Motivation = " + input.motivation
-            
-             inputsDiv.appendChild(inputsElement)
-         })
-    })
-
-window.addEventListener('beforeunload', function (e) {
-    e.preventDefault();
-    e.returnValue = '';
-    inputListener()
-});
+db.collection("answers").get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        renderAnswers(doc);        
+})
+})
+//////////////////////////////////////////////////
