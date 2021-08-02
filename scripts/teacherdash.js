af43a5a92baed2e7a1fc32a96416d65fb9b4bb05
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-01 20:34:13 -0400

    Update teacherdash.js

--- scripts/teacherdash.js
+++ scripts/teacherdash.js
@@ -1,1 +1,32 @@
+const inputsDiv = document.getElementById("inputs")
 
+const inputListener = firestore.collection("answers").doc("userId").collection("questions").doc("home")
+    .onSnapshot(doc => {
+        console.log("inputs listener")
+
+        if(!doc.exists)
+            return
+
+        inputsDiv.innerHTML = ""
+
+        let homeData = doc.data()
+
+        inputsDiv.innerHTML = "Name: " + homeData.name + ", " + homeData.motivation
+
+        // querySnapshot.forEach(doc => {
+        //     console.log(doc.data())
+
+        //     let input = doc.data()
+
+        //     let inputsElement = document.createElement("p")
+        //     inputsElement.innerHTML = "Name: " + input.name + " || Motivation = " + input.motivation
+            
+        //     inputsDiv.appendChild(inputsElement)
+        // })
+    })
+
+window.addEventListener('beforeunload', function (e) {
+    //e.preventDefault();
+    //e.returnValue = '';
+    inputListener()
+});
