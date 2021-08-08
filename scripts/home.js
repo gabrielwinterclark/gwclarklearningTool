59afe1ad7066d887d2378118da62084423d454df
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-08 19:46:11 -0400

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -1,32 +1,24 @@
-console.log("yeehaw 13")
-
-//home
-
+//grab "thankyou" from the HTML document so that we can change the message & confirm to the user that they have submitted on "submit"
 var thankyou = document.getElementById("thankyou");
 
-let form = document.getElementById("my-form")
+//grab the form for name and motivation
+let form = document.getElementById("my-form");
+
+//set up an event listener when the user hits submit on the form
 form.addEventListener("submit", () => {
-	event.preventDefault();
-	
+//remove there being a default answer even if the user has entered one on the page before
+    event.preventDefault();
+//show a message that says "thank you" to confirm to the user that they did submit the form
     thankyou.textContent="Thank you!";
-
-    //console.log(auth.currentUser.uid)
-
+//declare variables for the values that the user entered in the text boxes for name and motivation
     let name = document.getElementById("input-name").value;
     let motivation = document.getElementById("input-motivation").value;
-
-    console.log("name " + name);
-    console.log("motivation " + motivation);
-
-	    // firestore.collection("answers").doc("userId").collection("questions").doc("lo1-q1").set(q1)
-
+//first we "set" an array to hold the user's name, since the array didn't exist before and firebase needs to know where to put the info
     firestore.collection("answers").doc(auth.currentUser.uid).set({
             info: firebase.firestore.FieldValue.arrayUnion("Name: " + name),
-        })
-    
-	firestore.collection("answers").doc(auth.currentUser.uid).update({
+    });
+//then we "update" the array to avoid overwriting it, this time adding the user's motivation    
+    firestore.collection("answers").doc(auth.currentUser.uid).update({
 	    info: firebase.firestore.FieldValue.arrayUnion("Motivation: " + motivation),
-	})
-    // VHxJJc0cGOV0Xt7kH8ZTjAgdy6V2
-    
+    })    
 });
