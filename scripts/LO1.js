1dc753c2acb25773e07e1739c67ca7ce131611e0
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-08 17:29:09 -0400

    Update LO1.js

--- scripts/LO1.js
+++ scripts/LO1.js
@@ -17,10 +17,10 @@ form1.addEventListener("submit", () => {
     //return
 
     switch(value) {
-        case "Correct":
+        case "Learning Objective 1, Question 1: Correct (A)":
             q1feedback.innerHTML = "Correct! Yes, Jeremiah values playing the cello as an end, for itself."
             break;
-        case "Incorrect":
+        case "Learning Objective 1, Question 1: Incorrect (B)":
             q1feedback.innerHTML = "Not quite. Remember, something has intrinsic value when it is valuable for itself. Jeremiah does, in fact, value playing the cello for itself: he values it intrinsically."
             break;
     }
@@ -48,10 +48,10 @@ form2.addEventListener("submit", () => {
     //return
 
     switch(value) {
-        case "Incorrect":
+        case "Learning Objective 1, Question 2: Incorrect (A)":
             q2feedback.innerHTML = "Incorrect. Think about it: does May value playing the cello for itself, or for something else? In this case, it looks like May values playing the cello in service of something else. Read on to learn what type of value May assigns to playing the cello!"
             break;
-        case "Correct":
+        case "Learning Objective 1, Question 2: Correct (B)":
             q2feedback.innerHTML = "Correct! You may be wondering in what way May values playing the cello. Read on to find out."
             break;
     }
@@ -79,19 +79,19 @@ form3.addEventListener("submit", () => {
     //return
 
     switch(value) {
-        case "Incorrect (A)":
+        case "Learning Objective 1, Question 3: Incorrect (A)":
             q3feedback.innerHTML = "You are partially correct. Aminah does value her master’s degree instrumentally, as a means to move towards her future career. But, is there anyone else who also instrumentally values their degree?"
             break;
-        case "Incorrect (B)":
+        case "Learning Objective 1, Question 3: Incorrect (B)":
             q3feedback.innerHTML = "Incorrect. Phoebe values her master’s degree intrinsically for itself."
             break;
-        case "Incorrect (C)":
+        case "Learning Objective 1, Question 3: Incorrect (C)":
             q3feedback.innerHTML = "You are partially correct. Sam does value his master’s degree instrumentally, as a means to eventually write a book about environmental science. But, is there anyone else who also instrumentally values their degree? "
             break;
-        case "Correct (D)":
+        case "Learning Objective 1, Question 3: Correct (D)":
             q3feedback.innerHTML = "Correct! Yes, both Aminah and Sam value their degrees instrumentally, as a means to achieve their future goals."
             break;
-        case "Incorrect (E)":
+        case "Learning Objective 1, Question 3: Incorrect (E)":
             q3feedback.innerHTML = "Not quite. Not all of the students instrumentally value their degree as a means to some other end. Think about who might value their degree intrinsically."
             break;            
     }
