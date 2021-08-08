fcd59be971b8c74d97f9bfc056389d0bab8608b1
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-08 18:25:13 -0400

    Update teacherdash.js

--- scripts/teacherdash.js
+++ scripts/teacherdash.js
@@ -8,12 +8,54 @@ const results = document.getElementById('inputs');
 
 function renderAnswers(doc){
   let li = document.createElement('li');
-  let questionAnswers = document.createElement('span');
+  let name = document.createElement('span');
+  let motivation = document.createElement('span');
+  let LO1Q1 = document.createElement('span');
+  let LO1Q2 = document.createElement('span');
+  let LO1Q3 = document.createElement('span');
+  let LO2Q1 = document.createElement('span');
+  let LO2Q2 = document.createElement('span');
+  let LO2Q3 = document.createElement('span');
+  let LO3Q1 = document.createElement('span');
+  let LO3Q2 = document.createElement('span');
+  let LO3Q3 = document.createElement('span');
+  let assessment1 = document.createElement('span');
+  let assessment2 = document.createElement('span');
+  let assessment3 = document.createElement('span');
+  let space = document.createElement('span');
     
   li.setAttribute('data-id', doc.id);
-    questionAnswers.textContent = doc.data().assessment[0];
+  name.textContent = doc.data().assessment[0];
+  motivation.textContent = doc.data().assessment[0];
+  LO1Q1.textContent = doc.data().assessment[0];
+  LO1Q2.textContent = doc.data().assessment[0];
+  LO1Q3.textContent = doc.data().assessment[0];
+  LO2Q1.textContent = doc.data().assessment[0];
+  LO2Q2.textContent = doc.data().assessment[0];
+  LO2Q3.textContent = doc.data().assessment[0];
+  LO3Q1.textContent = doc.data().assessment[0];
+  LO3Q2.textContent = doc.data().assessment[0];
+  LO3Q3.textContent = doc.data().assessment[0];
+  assessment1.textContent = doc.data().assessment[0];
+  assessment2.textContent = doc.data().assessment[0];
+  assessment3.textContent = doc.data().assessment[0];
+  space.textContent = "";
     
-    li.appendChild(questionAnswers);
+  li.appendChild(name);
+  li.appendChild(motivation);
+  li.appendChild(LO1Q1);
+  li.appendChild(LO1Q2);
+  li.appendChild(LO1Q3);
+  li.appendChild(LO2Q1);
+  li.appendChild(LO2Q2);
+  li.appendChild(LO2Q3);
+  li.appendChild(LO3Q1);
+  li.appendChild(LO3Q2);
+  li.appendChild(LO3Q3);
+  li.appendChild(assessment1);
+  li.appendChild(assessment2);
+  li.appendChild(assessment3);
+  li.appendChild(space);
     
     results.appendChild(li);
     
