5f9a8676e44443e8b11041e31d034d7011777a93
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-08 15:52:55 -0400

    Update LO1.js

--- scripts/LO1.js
+++ scripts/LO1.js
@@ -1,4 +1,4 @@
-console.log("yeehaw 27")
+console.log("yeehaw 28")
 
 // https://github.com/gabrielwinterclark/learning-tool
 
@@ -27,7 +27,7 @@ form1.addEventListener("submit", () => {
 
     // firestore.collection("answers").doc("userId").collection("questions").doc("lo1-q1").set(q1)
     //add it here!! (the update elements in an array)
-    firestore.collection("answers").doc(auth.currentUser.uid).set({
+    firestore.collection("answers").doc(auth.currentUser.uid).update({
         questions: firebase.firestore.FieldValue.arrayUnion(value),
     })
 
