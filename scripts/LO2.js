cd9e640be168bc9ba7340041d056103024e9f7ad
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-08 17:45:55 -0400

    Update LO2.js

--- scripts/LO2.js
+++ scripts/LO2.js
@@ -17,17 +17,17 @@ form1.addEventListener("submit", () => {
     //return
 
     switch(value) {
-        case "Correct":
+        case "Learning Objective 2, Question 1: Correct (A)":
             q1feedback.innerHTML = "Correct! Yes, Jorge values BMWs for a nonrelational property: the well-made nature of the cars can’t be separated from them."
             break;
-        case "Incorrect":
+        case "Learning Objective 2, Question 1: Incorrect (B)":
             q1feedback.innerHTML = "Not quite. Remember, something has intrinsic value by this second definition when it is valued for its nonrelational properties, or those properties that can’t be separated from it. Is a car being well made separable from the car itself?"
             break;
     }
 
     // firestore.collection("answers").doc("userId").collection("questions").doc("lo1-q1").set(q1)
-    firestore.collection("answers").doc(auth.currentUser.uid).collection("questions").doc("lo2-q1").set({
-        q4: value,
+   firestore.collection("answers").doc(auth.currentUser.uid).update({
+        LO2: firebase.firestore.FieldValue.arrayUnion(value),
     })
 
 })
@@ -47,17 +47,17 @@ form2.addEventListener("submit", () => {
     //return
 
     switch(value) {
-        case "Incorrect":
+        case "Learning Objective 2, Question 2: Incorrect (A)":
             q2feedback.innerHTML = "Incorrect. Micah loves the movie because it makes them and their friends laugh. Is this a nonrelational property of the movie, or is it something that can be separated from the movie? "
             break;
-        case "Correct":
+        case "Learning Objective 2, Question 2: Correct (B)":
             q2feedback.innerHTML = "Correct! You may be wondering in what way Micah values Ratatouille. Read on to find out."
             break;
     }
 
     // firestore.collection("answers").doc("userId").collection("questions").doc("lo1-q1").set(q1)
-    firestore.collection("answers").doc(auth.currentUser.uid).collection("questions").doc("lo2-q2").set({
-        q5: value,
+    firestore.collection("answers").doc(auth.currentUser.uid).update({
+        LO2: firebase.firestore.FieldValue.arrayUnion(value),
     })
 
 })
@@ -77,17 +77,17 @@ form3.addEventListener("submit", () => {
     //return
 
     switch(value) {
-        case "Incorrect":
+        case "Learning Objective 2, Question 3: Incorrect (A)":
             q3feedback.innerHTML = "Incorrect. For Wes to value donating blood intrinsically in this way, he would have to value it for some nonrelational (inseparable) property. Is donating blood separable from the fact that it helps people in need? Remember, one way to test if something has intrinsic value is to think about it in a vacuum."
             break;
-        case "Correct":
+        case "Learning Objective 2, Question 3: Correct (B)":
             q3feedback.innerHTML = "Correct! Wes values donating blood extrinsically because he values it for the fact that it helps people, which is a relational (separable) property."
             break;      
     }
 
     // firestore.collection("answers").doc("userId").collection("questions").doc("lo1-q1").set(q1)
-    firestore.collection("answers").doc(auth.currentUser.uid).collection("questions").doc("lo2-q3").set({
-        q6: value,
+    firestore.collection("answers").doc(auth.currentUser.uid).update({
+        LO2: firebase.firestore.FieldValue.arrayUnion(value),
     })
 
 })
