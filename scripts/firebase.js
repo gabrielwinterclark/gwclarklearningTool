2dd5521f3fd17c39d01b02cf37f8bdf13ee88e8a
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-08 19:41:44 -0400

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -6,24 +6,20 @@ var firebaseConfig = {
     storageBucket: "learning-tool-5eb63.appspot.com",
     messagingSenderId: "128932611744",
     appId: "1:128932611744:web:626d046619028126fcebc5"
-}
+};
 
-firebase.initializeApp(firebaseConfig)
+firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth()
-const firestore = firebase.firestore()
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/firebase.User
       var uid = user.uid;
       console.log("Has user")
       console.log(user.uid)
       // ...
     } else {
-      // User is signed out
-      // ...
       console.log("No user")
     }
 });
