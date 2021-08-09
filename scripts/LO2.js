d2b29506cbfd83b067159afde75c2d1328aacafd
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-08-08 21:35:15 -0400

    Update LO2.js

--- scripts/LO2.js
+++ scripts/LO2.js
@@ -13,7 +13,7 @@ form1.addEventListener("submit", () => {
             q1feedback.innerHTML = "Correct! Yes, Jorge values BMWs for a nonrelational property: the well-made nature of the cars can’t be separated from them."
             break;
         case "Learning Objective 2, Question 1: Incorrect (B)":
-            q1feedback.innerHTML = "Not quite. Remember, something has intrinsic value by this second definition when it is valued for its nonrelational properties, or those properties that can’t be separated from it. Is a car being well made separable from the car itself? The correct answer is yes."
+            q1feedback.innerHTML = "Not quite. The correct answer here is yes. Remember, something has intrinsic value by this second definition when it is valued for its nonrelational properties, or those properties that can’t be separated from it. Is a car being well made separable from the car itself?"
             break;
     };
     firestore.collection("answers").doc(auth.currentUser.uid).update({
@@ -33,7 +33,7 @@ form2.addEventListener("submit", () => {
     let value = document.querySelector('input[name="lo2-q2-answer"]:checked').value;
     switch(value) {
         case "Learning Objective 2, Question 2: Incorrect (A)":
-            q2feedback.innerHTML = "Incorrect. Micah loves the movie because it makes them and their friends laugh. Is this a nonrelational property of the movie, or is it something that can be separated from the movie? The correct answer is no."
+            q2feedback.innerHTML = "Incorrect. The correct answer here is no. Micah loves the movie because it makes them and their friends laugh. Is this a nonrelational property of the movie, or is it something that can be separated from the movie?"
             break;
         case "Learning Objective 2, Question 2: Correct (B)":
             q2feedback.innerHTML = "Correct! You may be wondering in what way Micah values Ratatouille. Read on to find out."
@@ -56,7 +56,7 @@ form3.addEventListener("submit", () => {
     let value = document.querySelector('input[name="lo2-q3-answer"]:checked').value;
     switch(value) {
         case "Learning Objective 2, Question 3: Incorrect (A)":
-            q3feedback.innerHTML = "Incorrect. For Wes to value donating blood intrinsically in this way, he would have to value it for some nonrelational (inseparable) property. Is donating blood separable from the fact that it helps people in need? Remember, one way to test if something has intrinsic value is to think about it in a vacuum. The correct answer is extrincisally."
+            q3feedback.innerHTML = "Incorrect. The correct answer is extrincisally. For Wes to value donating blood intrinsically, he would have to value it for some nonrelational (inseparable) property. Is donating blood separable from the fact that it helps people in need? Remember, one way to test if something has intrinsic value is to think about it in a vacuum."
             break;
         case "Learning Objective 2, Question 3: Correct (B)":
             q3feedback.innerHTML = "Correct! Wes values donating blood extrinsically because he values it for the fact that it helps people, which is a relational (separable) property."
